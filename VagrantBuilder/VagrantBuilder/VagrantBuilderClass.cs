57c5b9f936eb07abda3994ec512fa55cb52ffd3f
jhallam3 <jon.hallam@ManagedDesktop.com>
2019-04-25 14:16:32 +0100

    Initial Commit

---
+++ VagrantBuilder/VagrantBuilder/VagrantBuilderClass.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using VagrantBuilder.Classes;
+
+
+namespace VagrantBuilder
+{
+    public class VagrantBuilderClass
+    {
+        public string GetReleases (string pathToFile)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(pathToFile);
+            var response = (HttpWebResponse)request.GetResponse();
+            string responseString;
+            using (var stream = response.GetResponseStream())
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    responseString = reader.ReadToEnd();
+                }
+            }
+            return responseString;
+        }
+        public BuildList ToXml (string xmlAsString)
+        {
+           
+            var serializer = new XmlSerializer(typeof(BuildList));
+
+            BuildList _BuildList;
+
+            using (var reader = new StringReader(xmlAsString))
+            {
+                 _BuildList = (BuildList)serializer.Deserialize(reader);
+            }
+            return _BuildList;
+        }
+
+        public string MakeDir (string BuildNumber)
+        {
+            var defaultroot = @"C:\Vagrant\Decisions";
+            var pathtoBuild = defaultroot + BuildNumber;
+            var exists = Directory.Exists(pathtoBuild);
+            if (exists == true)
+            {
+                DateTime date = DateTime.Now;
+                var newpathtobuild = pathtoBuild+" DUPE "+ + date.Month + date.Day + date.Second;
+                Directory.CreateDirectory(newpathtobuild);
+                return newpathtobuild;
+            }
+            else
+            {
+                Directory.CreateDirectory(pathtoBuild);
+                return pathtoBuild;
+            }
+            
+        }
+
+        public void CloneBaseFIles(string unzipToPath)
+        {
+            var zippedfiles = Directory.GetCurrentDirectory() + @"\Content\Archive.zip";
+            zippedfiles = zippedfiles.Replace("Console", string.Empty);
+            var exists = System.IO.File.Exists(zippedfiles);
+            System.IO.Compression.ZipFile.ExtractToDirectory(zippedfiles, unzipToPath);
+
+        }
+
+        public bool UpdateDownloadStringInDecisionsAutoInstaller(string PathToRootOfBuildFolder, string BuildNumber)
+        {
+
+
+            try
+            {
+                var FileTochangeFullPath = PathToRootOfBuildFolder + @"\DecisionsInstaller\InstallerSetup.xml";
+                var filesContent = File.ReadAllText(FileTochangeFullPath);
+                filesContent = filesContent.Replace("<TxtAlternateURLSource>https://releases.decisions.com/releasedversions/60577</TxtAlternateURLSource>", "<TxtAlternateURLSource>https://releases.decisions.com/releasedversions/"+BuildNumber+"</TxtAlternateURLSource>");
+                System.IO.File.WriteAllText(FileTochangeFullPath, filesContent);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+               
+            }
+        }
+    }
+}
---
+++ VagrantBuilder/VagrantBuilderConsole/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VagrantBuilder.Classes;
+
+namespace VagrantBuilderConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                var xmlasstring = new VagrantBuilder.VagrantBuilderClass().GetReleases("https://releases.decisions.com/releasedversions/PublicReleases.xml");
+
+
+
+                var xml = new VagrantBuilder.VagrantBuilderClass().ToXml(xmlasstring);
+
+                IEnumerable<BuildListBuild> buildListBuilds = xml.Builds.OrderBy(x => x.SVNRevision).ToList();
+
+                foreach (var i in buildListBuilds)
+                {
+                    Console.WriteLine(i.SVNRevision + " Released on the " + i.ReleasedOn);
+                }
+
+                Console.WriteLine("Which Build Number??");
+                string buildNumber = Console.ReadLine();
+
+                var makeddir = new VagrantBuilder.VagrantBuilderClass().MakeDir(buildNumber);
+                new VagrantBuilder.VagrantBuilderClass().CloneBaseFIles(makeddir);
+                new VagrantBuilder.VagrantBuilderClass().UpdateDownloadStringInDecisionsAutoInstaller(makeddir, buildNumber);
+
+
+                Console.WriteLine("-----");
+                Console.WriteLine("Done Full Path is " + makeddir);
+                Console.WriteLine("-----");
+                Console.WriteLine("IN CMD (Run as Administrator)");
+                Console.WriteLine("CD "+makeddir);
+                Console.WriteLine("vagrant up --provider hyperv");
+                     
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
