34c714ca5bfd16971ae700071bc8d0221d317a78
jon <jon.hallam@managed-desktop.com>
2019-09-05 14:18:19 +0100

    Now working for any OS,

--- VagrantBuilder/VagrantBuilder/VagrantBuilderClass.cs
+++ VagrantBuilder/VagrantBuilder/VagrantBuilderClass.cs
@@ -8,10 +8,10 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
-using VagrantBuilderCore.Classes;
 
 
-namespace VagrantBuilderCore
+
+namespace VagrantBuilder
 {
     public class VagrantBuilderClass
     {
@@ -157,5 +157,167 @@ namespace VagrantBuilderCore
                
             }
         }
-    }
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+        [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+        public partial class BuildList
+        {
+
+            private BuildListBuild[] buildsField;
+
+            /// <remarks/>
+            [System.Xml.Serialization.XmlArrayItemAttribute("Build", IsNullable = false)]
+            public BuildListBuild[] Builds
+            {
+                get
+                {
+                    return this.buildsField;
+                }
+                set
+                {
+                    this.buildsField = value;
+                }
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+        public partial class BuildListBuild
+        {
+
+            private string releasedNameField;
+
+            private string stagedNameField;
+
+            private string fullVersionField;
+
+            private string sVNRevisionField;
+
+            private string pathField;
+
+            private string releasedByField;
+
+            private string releasedOnField;
+
+            private bool isPublicallyReleasedField;
+
+            private string buildFamilyField;
+
+            /// <remarks/>
+            public string ReleasedName
+            {
+                get
+                {
+                    return this.releasedNameField;
+                }
+                set
+                {
+                    this.releasedNameField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string StagedName
+            {
+                get
+                {
+                    return this.stagedNameField;
+                }
+                set
+                {
+                    this.stagedNameField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string FullVersion
+            {
+                get
+                {
+                    return this.fullVersionField;
+                }
+                set
+                {
+                    this.fullVersionField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string SVNRevision
+            {
+                get
+                {
+                    return this.sVNRevisionField;
+                }
+                set
+                {
+                    this.sVNRevisionField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string Path
+            {
+                get
+                {
+                    return this.pathField;
+                }
+                set
+                {
+                    this.pathField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string ReleasedBy
+            {
+                get
+                {
+                    return this.releasedByField;
+                }
+                set
+                {
+                    this.releasedByField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string ReleasedOn
+            {
+                get
+                {
+                    return this.releasedOnField;
+                }
+                set
+                {
+                    this.releasedOnField = value;
+                }
+            }
+
+            /// <remarks/>
+            public bool IsPublicallyReleased
+            {
+                get
+                {
+                    return this.isPublicallyReleasedField;
+                }
+                set
+                {
+                    this.isPublicallyReleasedField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string BuildFamily
+            {
+                get
+                {
+                    return this.buildFamilyField;
+                }
+                set
+                {
+                    this.buildFamilyField = value;
+                }
+            }
+        }
+        }
 }
