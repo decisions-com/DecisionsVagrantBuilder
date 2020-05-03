abc594cec35628b57571f98323abe7acb667abac
jon <jon.hallam@managed-desktop.com>
2020-05-03 16:05:22 +0100

    Added run commandine program

--- VagrantBuilder/VagrantBuilderMakeFiles/Program.cs
+++ VagrantBuilder/VagrantBuilderMakeFiles/Program.cs
@@ -9,8 +9,27 @@ namespace VagrantBuilderMakeFiles
         static void Main(string[] args)
         {
             //dotnet C:\\Vagrant\\code\\DecisionsVagrantBuilder\\VagrantBuilder\\VagrantBuilderConsoleRunAnywhere\\bin\\Debug\\netcoreapp2.1\\VagrantBuilderConsoleRunAnywhere.dll Rundeck:${ option.Version}:${ option.Customer}\${ option.ServerInstance}
+            try
+            {
+                Console.WriteLine("CustomerName, InstanceName, CPU, RAM(gigs), Version, BaseURL, OutputPathOfFile");
+                string CustomerName = args[0];
 
-            var f = new VagrantBuild().RunnerHyperV("Jon", "instance1", 4, 16, 64509, "http://localhost", @"C:\vagrant\output");
+                string InstanceName = args[1];
+                int CPU = int.Parse(args[2]);
+                int RAM = int.Parse(args[3]);
+                int Version = int.Parse(args[4]);
+                string BaseURL = args[5];
+                string OutputPathOfFile = args[6];
+
+
+                var f = new VagrantBuild().RunnerHyperV(CustomerName,InstanceName, CPU, RAM, Version, BaseURL, OutputPathOfFile);
+            }
+            catch (Exception ex2)
+            {
+                Console.WriteLine(ex2.Message);
+                Console.WriteLine(ex2.InnerException);
+                throw;
+            }
         }
     }
 }
